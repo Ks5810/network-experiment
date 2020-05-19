361695b207f1614f2a25f1048fe56952bda9ff4c
Ks5810 <e40keisuke@gmail.com>
2020-05-19 04:18:47 -0400

    utils.cpp utils::add() checking overflow with negative inputs

--- src/utils.cpp
+++ src/utils.cpp
@@ -14,15 +14,20 @@ Modification    :
 
 int utils::add(int lhs, int rhs)
 {
-    if(lhs > INT_MAX - rhs)
+    if(((rhs > 0) && (lhs > (INT_MAX - rhs))) ||
+       ((rhs < 0) && (lhs < (INT_MIN - rhs))))
+    {
         return INT_MAX;
+    }
     else
+    {
         return lhs + rhs;
+    }
 }
 
 void utils::printWithSpaceBetween(const string &lhs, int rhs, int width)
 {
-    width = width / 2 - 1;
+    width = width/2 - 1;
     cout << left << setfill(' ') << setw(width) << lhs
          << right << setw(width) << rhs << "\n";
 }
@@ -31,18 +36,20 @@ void utils::printCentered(string str, int width)
 {
     int lhs = 2*width/3, rhs = width/3;
     
-    if(width%3 == 1) {
-        lhs = width%2 == 0 ? lhs-4 : lhs-3;
-        rhs+=4;
-        
-    } else if(width%3 == 2)
+    if(width%3 == 1)
     {
-        lhs-=4;
-        rhs = width%2 == 0 ? rhs+4 : rhs+1;
-    } else
+        lhs = width%2 == 0 ? lhs - 4 : lhs - 3;
+        rhs += 4;
+    }
+    else if(width%3 == 2)
     {
-        lhs = width%2 == 0 ? lhs-4 : lhs-3;
-        rhs = width%2 == 0 ? rhs+3 : rhs+1;
+        lhs -= 4;
+        rhs = width%2 == 0 ? rhs + 4 : rhs + 1;
+    }
+    else
+    {
+        lhs = width%2 == 0 ? lhs - 4 : lhs - 3;
+        rhs = width%2 == 0 ? rhs + 3 : rhs + 1;
     }
     
     string tmp { str.empty() ? str : " "s.append(str.append(" ")) };
